c6e5be206b4050aeca5465fba77bc8a989424090
KramPhoenix <56003203+KramPhoenix@users.noreply.github.com>
2020-05-24 20:43:40 +0200

    Layout reformed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,23 +21,35 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+        <nav id="layout-nav" class="navbar navbar-expand-md navbar-light shadow-sm">
+            <div class="container d-flex justify-content-around">
+                <a id="layout-brand" class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div id="layout-search" class="input-group md-form form-sm form-2 pl-0">
+                    <input class="form-control my-0 py-1 lime-border" type="text" placeholder="Search" aria-label="Search">
+                    <div class="input-group-append">
+                        <span class="input-group-text lime lighten-2" id="basic-text1">
+                            <svg class="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                              <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="navbar" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul id="layout-nav-menu" class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
@@ -72,9 +84,33 @@
             </div>
         </nav>
 
+        <nav id="layout-nav2" class="navbar navbar-expand-md navbar-light shadow-sm">
+            <ul class="d-flex justify-content-around align-items-center">
+                <li><div class="dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Categorías
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div></li>
+                <li><a href="">Notícias</a></li>
+                <li><a href="">Lo más vendido</a></li>
+                <li><a href="">Ofertas</a></li>
+                <li><a href="">FAQS</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="footer">
+
+        </footer>
     </div>
 </body>
 </html>
