022c77157bf51b29dcb8e1d6d7392321577bf495
KramPhoenix <56003203+KramPhoenix@users.noreply.github.com>
2020-05-25 00:10:23 +0200

    Migraciones funcionales

---
+++ database/migrations/2020_05_24_185114_products.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->string('description');
+            $table->decimal('last_price');
+            $table->decimal('price');
+            $table->bigInteger('brand_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('offer_id')->nullable()->unsigned();
+            $table->timestamps();
+
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('offer_id')->references('id')->on('offers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
